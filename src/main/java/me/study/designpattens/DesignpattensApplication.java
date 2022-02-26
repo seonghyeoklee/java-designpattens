9b3e6b9db667ba711a5a3ca4113a21baa1067301
seonghyeoklee <dltjdgur327@gmail.com>
2022-02-26 14:16:02 +0900

    init project

---
+++ src/main/java/me/study/designpattens/DesignpattensApplication.java
@@ -0,0 +1,13 @@
+package me.study.designpattens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpattensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignpattensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/study/designpattens/DesignpattensApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.designpattens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpattensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
