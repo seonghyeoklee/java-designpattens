5a9aeaee4199f96518c563fba60232feeb303011
seonghyeoklee <dltjdgur327@gmail.com>
2022-02-26 15:09:56 +0900

    리플렉션의 사용

--- src/main/java/me/study/designpattens/singleton/App.java
+++ src/main/java/me/study/designpattens/singleton/App.java
@@ -1,9 +1,15 @@
 package me.study.designpattens.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings1 = Settings.getInstance();
-        Settings settings2 = Settings.getInstance();
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings2 = constructor.newInstance();
 
         System.out.println(settings1.equals(settings2));
     }
